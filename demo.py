b53ccab570c42c59beeef6a90711db65c0907818
nishad10 <nishad.aherrao10@gmail.com>
2020-12-02 20:41:09 -0600

    Added email option to send alert through email to user

--- demo.py
+++ demo.py
@@ -1,14 +1,27 @@
-from PIL import Image
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
-from tkinter.filedialog import askopenfilename
 import face_recognition
 import os
 import shutil
+import smtplib
+import ssl
+from PIL import Image
+from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
+
+port = 587  # For starttls
+smtp_server = "smtp.gmail.com"
+sender_email = "nishad10dev@gmail.com"
+receiver_email = "nishad.aherrao10@gmail.com"
+password = input("Type your password and press enter:")
+message = """\
+Subject: Face Recognition Security Alert
+
+ALERT!! UNKNOWN FACE FOUND!"""
+
 
 # Load the jpg file into a numpy array
 user_image = face_recognition.load_image_file("./known_faces/nishad.jpg")
 unknown_image = face_recognition.load_image_file(
-    "./unknown_faces/nishad_stranger.jpg")
+    "./unknown_faces/biden.jpg")
 
 # Known user face encoding
 user_encoding = face_recognition.face_encodings(user_image)[0]
@@ -27,7 +40,7 @@ for face_encoding in face_encodings:
         known_faces_encoding, face_encoding)
     results.append(result)
 
-results = [item for sublist in results for item in sublist]
+results = [item for sublist in results for item in sublist]  # Flatten array
 print(results)
 ###############################
 # Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
@@ -44,6 +57,11 @@ if False in results:
     if True not in results:
         # This means theres all unknown faces in picture
         print('ALERT!! UNKNOWN FACE FOUND!')
+        context = ssl.create_default_context()
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.starttls(context=context)
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
 else:
     # This means all known faces in picture
     print('Hello! This will not be alerted')
