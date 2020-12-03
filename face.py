8d4b3c063e175666c2841f2d24af92746f3dd385
nishad10 <nishad.aherrao10@gmail.com>
2020-12-03 00:32:32 -0600

    Added timer for email and fixed alert system

--- face.py
+++ face.py
@@ -3,7 +3,10 @@ import os
 import shutil
 import smtplib
 import ssl
-#from PIL import Image
+import cv2
+import numpy as np
+import time
+
 # from tkinter import Tk     # from tkinter import Tk for Python 3.x
 #from tkinter.filedialog import askopenfilename
 
@@ -11,34 +14,26 @@ port = 587  # For starttls
 smtp_server = "smtp.gmail.com"
 sender_email = "nishad10dev@gmail.com"
 receiver_email = "nishad.aherrao10@gmail.com"
-#password = input("Type your password and press enter:")
+password = input("Type your password and press enter:")
+
 message = """\
 Subject: Face Recognition Security Alert
 
 ALERT!! UNKNOWN FACE FOUND!"""
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-# Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
 known_face_encodings = []
 known_face_names = []
 directory = './known_faces'
 for filename in os.listdir(directory):
+    oldfilename = filename
     filename = './known_faces/' + filename
     if filename.endswith(".jpg") or filename.endswith(".png"):
         user_image = face_recognition.load_image_file(filename)
         user_image_encoding = face_recognition.face_encodings(user_image)[0]
         known_face_encodings.append(user_image_encoding)
-        known_face_names.append(filename)
+        known_face_names.append(oldfilename)
     else:
         continue
 
@@ -47,16 +42,18 @@ face_locations = []
 face_encodings = []
 face_names = []
 process_this_frame = True
+sendEmail = True
+startTime = time.time()
 
 while True:
+    sendEmail = time.time() - startTime > 60
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
     # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
+    rgb_small_frame = frame[:, :, ::-1]
 
     # Only process every other frame of video to save time
     if process_this_frame:
@@ -71,21 +68,23 @@ while True:
             matches = face_recognition.compare_faces(
                 known_face_encodings, face_encoding)
             name = "Unknown"
-            if False in matches:
-                # This means there is unknown face in our picture
-                if True in matches:
-                    # This means theres a known face as well as a unknown face so its ok
-                    print(
-                        'Ignoring alert as unknown face is accompanied by known face.')
-                if True not in matches:
-                    # This means theres all unknown faces in picture
-                    print('ALERT!! UNKNOWN FACE FOUND!')
-                    #context = ssl.create_default_context()
-                    # with smtplib.SMTP(smtp_server, port) as server:
-                    # server.starttls(context=context)
-                    #server.login(sender_email, password)
-                    #server.sendmail(sender_email, receiver_email, message)
-            # # If a match was found in known_face_encodings, just use the first one.
+            # f t f
+            print(matches)
+            if True in matches:
+                # This means theres a known face so its ok
+                print(
+                    'Ignoring alert as known face is in picture')
+            if True not in matches:
+                # This means theres all unknown faces in picture
+                print('ALERT!! UNKNOWN FACE FOUND!')
+                if sendEmail:
+                    startTime = time.time()
+                    context = ssl.create_default_context()
+                    with smtplib.SMTP(smtp_server, port) as server:
+                        server.starttls(context=context)
+                        server.login(sender_email, password)
+                        server.sendmail(sender_email, receiver_email, message)
+            # If a match was found in known_face_encodings, just use the first one.
             # if True in matches:
             #     first_match_index = matches.index(True)
             #     name = known_face_names[first_match_index]
@@ -98,29 +97,29 @@ while True:
                 name = known_face_names[best_match_index]
 
             face_names.append(name)
-
     process_this_frame = not process_this_frame
 
     # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+    try:
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35),
-                      (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6),
-                    font, 1.0, (255, 255, 255), 1)
-
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35),
+                          (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6),
+                        font, 1.0, (255, 255, 255), 1)
+    finally:
+        cv2.imshow('Video', frame)
     # Display the resulting image
-    cv2.imshow('Video', frame)
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
